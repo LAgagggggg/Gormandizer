13a23b3304841d6f3a28851cb196e2693d25ecaf
LAgagggggg <15807814960@163.com>
2020-06-10 14:17:04 +0800

    improved UI

--- src/index.js
+++ src/index.js
@@ -8,9 +8,54 @@ const request = require('request');
 const backendURL = 'http://127.0.0.1:3001';
 
 class EditSection extends React.Component {
+    render() {
+        return (
+            <div>
+                <Input.TextArea
+                    className="main-input"
+                    value={this.props.textContent}
+                    onChange={this.props.handleChange}
+                    placeholder='请输入需要保存的内容' >
+
+                </Input.TextArea>
+                <Button type="primary"
+                    size='large'
+                    block={true}
+                    loading={this.props.isSubmitting}
+                    onClick={this.props.handleSubmit}
+                    disabled={this.props.textContent.length === 0}
+                    style={{ marginTop: "10px", borderRadius: "10px" }}>
+                    Commit
+                </Button>
+            </div>
+        );
+    }
+}
+
+class History extends React.Component {
+    render() {
+        const timeLineItems = this.props.history.map((content) => {
+            return (
+                <Timeline.Item>
+                    <Button>
+                        {content}
+                    </Button>
+                </Timeline.Item>
+            )
+        });
+        return (
+            <Timeline className='main-timeline' reverse={true}>
+                {timeLineItems}
+            </Timeline>
+        )
+    }
+}
+
+class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            history: [],
             textContent: "",
             isSubmitting: false
         };
@@ -23,51 +68,23 @@ class EditSection extends React.Component {
     handleSubmit() {
         this.setState({ isSubmitting: true })
         request.post({
-            url:backendURL + '/commit',
+            url: backendURL + '/commit',
             method: 'POST',
             headers: {
                 "content-type": "application/json",
             },
-            body: JSON.stringify({content: this.state.textContent}) 
+            body: JSON.stringify({ content: this.state.textContent })
         }, (err, res, body) => {
-            this.setState({ isSubmitting: false })
+            this.setState({ 
+                isSubmitting: false,
+                textContent: ""
+            })
             if (err) { return console.log(err); }
+            this.reloadHistory()
         });
     }
 
-    render() {
-        return (
-            <div>
-                <Input.TextArea
-                    className="main-input"
-                    value={this.state.textContent}
-                    onChange={(e) => this.handleChange(e)}
-                    placeholder='请输入需要保存的内容' >
-
-                </Input.TextArea>
-                <Button type="primary"
-                    size='large'
-                    block={true}
-                    loading={this.state.isSubmitting}
-                    onClick={()=>this.handleSubmit()}
-                    disabled={this.state.textContent.length === 0}
-                    style={{ marginTop: "10px", borderRadius: "10px" }}>
-                    Commit
-                </Button>
-            </div>
-        );
-    }
-}
-
-class History extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            history: []
-        };
-    }
-
-    componentDidMount() {
+    reloadHistory() {
         request.get(backendURL + '/history', (err, res, body) => {
             if (err) { return console.log(err); };
             let history = JSON.parse(body);
@@ -80,37 +97,27 @@ class History extends React.Component {
         });
     }
 
-    render() {
-        const timeLineItems = this.state.history.map((content) => {
-            return (
-            <Timeline.Item>
-            <Button>
-                {content}
-            </Button>
-            </Timeline.Item>
-            )
-        });
-        return (
-            <Timeline className='main-timeline'>
-                {timeLineItems}
-            </Timeline>
-        )
+    componentDidMount() {
+        this.reloadHistory();
     }
-}
 
-class App extends React.Component {
     render() {
         return (
             <div className="gormandizer-main">
                 <Row gutter={30} className='main-grid'>
                     <Col span={12} style={{ marginTop: "35px" }}>
-                        <EditSection />
+                        <EditSection 
+                            textContent={this.state.textContent}
+                            handleSubmit={()=>this.handleSubmit()}
+                            handleChange={(e) => this.handleChange(e)} 
+                            isSubmitting={this.state.isSubmitting}
+                        />
                     </Col>
                     <Col span={12}>
                         <Space direction="vertical">
                             <b>History</b>
                             <b></b>
-                            <History/>
+                            <History history={this.state.history} />
                         </Space>
                     </Col>
                 </Row>
